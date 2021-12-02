0f7e2d5499f81f29e4624421c7aee6772952ca4c
kali <sharktallica@gmail.com>
2021-12-02 21:49:53 +0000

    Complete Globals, Helpers and BasicHTMLWraps files

---
+++ resources/applets/Globals.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+    SharkblockX Globals
+    SharktasticA 2021
+*/
+
+/**
+ * The server's document root.
+ */
+$SRV_ROOT = $_SERVER["DOCUMENT_ROOT"]."/";
+
+/**
+ * The directory containing this application's CSS files.
+ */
+$CSS_ROOT = "resources/styles/";
+
+/**
+ * The directory containing this application's JavaScript
+ * files.
+ */
+$JS_ROOT = "resources/scripts/";
+
+/**
+ * The placeholder image filepath used when a non-existent
+ * image is found.
+ */
+$NO_IMG_PLACEHOLDER = "resources/images/misc/null.png";
