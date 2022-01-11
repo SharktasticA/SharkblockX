69f81010cf3179c017369b7eff7bc82c39f0c53f
kali <sharktallica@gmail.com>
2022-01-11 21:37:39 +0000

    Test page now shows all possible errors

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
 <?php
+        
+ini_set("display_errors", 1);
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
 
 include($_SERVER["DOCUMENT_ROOT"].'/resources/applets/SharkblockX.php');
 
-$page = new SbXProcessor("Test", "en-GB", "UTF-8", "http://localhost:80/", false, true);
+$page = new SbXProcessor("Test", "en-gb", "UTF-8", "http://localhost:80/", false, true);
 
 $page->draw();
